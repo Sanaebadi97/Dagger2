e64701a6817eb2391f953480cdacc8a3d7277c23
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-01 10:57:33 -0400

    added @provider module

--- app/src/main/java/info/sanaebadi/dagger2/medium/module/InfoModule.kt
+++ app/src/main/java/info/sanaebadi/dagger2/medium/module/InfoModule.kt
@@ -1,2 +1,15 @@
 package info.sanaebadi.dagger2.medium.module
 
+import dagger.Module
+import dagger.Provides
+import info.sanaebadi.dagger2.medium.model.Info
+
+
+@Module
+class InfoModule {
+
+    @Provides
+    fun sayLoveDagger2():Info{
+        return Info("Love Dagger 2")
+    }
+}
