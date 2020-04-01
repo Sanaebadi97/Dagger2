7d6010f1cb3c211a1fd1df5f50caf2ec96fc9da0
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-01 10:25:19 -0400

    created some method in interface

--- app/src/main/java/info/sanaebadi/dagger2/medium/component/MagicBox.kt
+++ app/src/main/java/info/sanaebadi/dagger2/medium/component/MagicBox.kt
@@ -1,9 +1,12 @@
 package info.sanaebadi.dagger2.medium.component
 
 import dagger.Component
+import info.sanaebadi.dagger2.medium.Main3Activity
 
 
 @Component
 interface MagicBox{
+
+    fun poke(app:Main3Activity)
 
 }
