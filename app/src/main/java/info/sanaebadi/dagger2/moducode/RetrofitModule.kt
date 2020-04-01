f10ebe7a8017ace11de3d419db0be26f1e2faa95
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-01 12:50:38 -0400

    added some dependencies

--- app/src/main/java/info/sanaebadi/dagger2/moducode/RetrofitModule.kt
+++ app/src/main/java/info/sanaebadi/dagger2/moducode/RetrofitModule.kt
@@ -4,6 +4,7 @@ import dagger.Provides
 import okhttp3.Cache
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -29,5 +30,10 @@ class RetrofitModule {
             .cache(cache)
             .build()
 
+
+    fun provideInterceptor():Interceptor= HttpLoggingInterceptor(HttpLoggingInterceptor.Logger{
+        Time
+
+    }
 
 }
