55a1aedcbb180aef8630373307af80358fecec94
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-01 12:29:56 -0400

    added some rx dependencies

--- app/src/main/java/info/sanaebadi/dagger2/moducode/RetrofitModule.kt
+++ app/src/main/java/info/sanaebadi/dagger2/moducode/RetrofitModule.kt
@@ -1,2 +1,17 @@
 package info.sanaebadi.dagger2.moducode
 
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+
+class RetrofitModule {
+
+
+    @Provides
+    fun provideRetrofit(httpClient: OkHttpClient):Retrofit=
+        Retrofit.Builder()
+            .client(httpClient)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+}
