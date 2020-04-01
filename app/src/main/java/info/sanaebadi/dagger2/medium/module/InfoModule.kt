a3d63a2223bfa91c7f94cf502f826c80a98be67e
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-01 11:34:49 -0400

    done

--- app/src/main/java/info/sanaebadi/dagger2/medium/module/InfoModule.kt
+++ app/src/main/java/info/sanaebadi/dagger2/medium/module/InfoModule.kt
@@ -9,7 +9,13 @@ import info.sanaebadi.dagger2.medium.model.Info
 class InfoModule {
 
     @Provides
-    fun sayLoveDagger2():Info{
+    fun sayLoveDagger2(): Info {
         return Info("Love Dagger 2")
+    }
+
+
+    @Provides
+    fun sayHelloDagger2(): Info {
+        return Info("Hello Dagger 2")
     }
 }
